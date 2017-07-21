6788ede5eb5a0c34554774949ca1f74aa88aa0f2
Unknown <peter-zobel@web.de>
2017-07-21 19:20:33 +0200

    Update EllipsoidTest

--- Test/EllipsoidPosition_test.py
+++ Test/EllipsoidPosition_test.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import patch
 from math import pi
-from EllipsoidPosition import EllipsoidPosition
+from Position import EllipsoidPosition
 from MathLib import toVector
 from Velocity import Velocity
 from numpy import rad2deg, deg2rad
@@ -16,21 +16,21 @@ class Position_test(unittest.TestCase):
         self.assertIsInstance(p.a, (int, float))
         self.assertIsInstance(p.f, (int, float))
         
-    @patch('EllipsoidPosition.DT',0.1)    
+    #@patch('Position.DT',0.1)    
     def test_update(self):
         pos = toVector(deg2rad(10.), deg2rad(200.), 0.)
         vel = Velocity(toVector(50000., 40000., 10000.))
         p = EllipsoidPosition(pos)
-        p.update(vel)
+        p.update(vel,0.1)
         self.assertAlmostEqual(10.04520478, rad2deg(p.values[0]), delta = 1e-8) 
         self.assertAlmostEqual(200.03538314, rad2deg(p.values[1]), delta = 1e-8)
         self.assertEqual(1000., p.values[2])
     
-    @patch('EllipsoidPosition.DT',0.1)            
+    #@patch('Position.DT',0.1)            
     def test_update_2(self):
         p = EllipsoidPosition(toVector(deg2rad(51.),deg2rad(10.),0.))
         vel = Velocity(toVector(10.,20.,0.)) #equal to 1 and 2 meters 
-        p.update(vel)
+        p.update(vel,0.1)
         self.assertAlmostEqual(51.+8.988903268e-06,rad2deg(p.values[0].item()), delta =10e-06) 
         self.assertAlmostEqual(10.+1.28368253e-05,rad2deg(p.values[1]), delta = 10e-06) 
         
